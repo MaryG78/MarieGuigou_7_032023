1f93670f4372b37d2902eaf1fc61595812a2632a
Marie Guigou <guigoumarie@gmail.com>
2023-03-14 09:25:34 +0100

    Creating the router and the pages of the application

--- kasa/src/index.js
+++ kasa/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./styles/index.scss";
-import { BrowserRouter } from "react-router-dom";
+
 
 
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ kasa/src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            About page works
+        </div>
+    );
+};
+
+export default About;
