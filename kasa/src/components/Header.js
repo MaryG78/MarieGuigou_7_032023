58fe67ae5084149d6fbf95d22819270ed1173977
Marie Guigou <guigoumarie@gmail.com>
2023-03-14 10:23:01 +0100

    setting up a layout for navigation

--- kasa/src/components/Header.js
+++ kasa/src/components/Header.js
@@ -1,14 +1,29 @@
-// import React from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
-// const Header = () => {
-//   return (
-//     <>
-//       <div className="header-container">
-//         <h1>Je suis le Header</h1>
-//       </div>
-//       <p>test</p>
-//     </>
-//   );
-// };
+const Header = () => {
+  return (
+    <>
+        <header className="header-container">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/home">Accueil</Link>
+              </li>
+              <li>
+                <Link to="/accomodation">Logements</Link>
+              </li>
+              <li>
+                <Link to="/about">A propos</Link>
+              </li>
+              <li>
+                <Link to="/error">Erreur 404</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+    </>
+  );
+};
 
-// export default Header;
+export default Header;
--- kasa/src/components/Router.js
+++ kasa/src/components/Router.js
@@ -3,17 +3,20 @@ import Error from "../pages/error";
 import About from "../pages/about";
 import Accomodation from "../pages/accomodation";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../pages/layout";
 
 // création d'un composant de routage
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/accomodation" element={<Accomodation />} />
-        <Route path="*" element={<Error />} />
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/accomodation" element={<Accomodation />} />
+          <Route path="*" element={<Error />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
